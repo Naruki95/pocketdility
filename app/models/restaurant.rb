f4a24dd4dafb23ba637ebc8f9afd05723241e6b4
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-03-09 09:09:04 +0100

    added a qr_code method to restaurant that shows qr_code in the relevant view

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,16 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   belongs_to :speciality
+
+  def qr_code
+    path_to_self = Rails.application.routes.url_helpers.restaurant_path(self)
+    RQRCode::QRCode.new(path_to_self)
+                   .as_svg(
+                     color: "000",
+                     shape_rendering: "crispEdges",
+                     module_size: 5,
+                     standalone: true,
+                     use_path: true
+                   )
+  end
 end
